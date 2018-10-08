bc615bf3f8cf9b71901593acde744c6ee39c1cfe
Jan Pipek <jan.pipek@showmax.com>
2018-10-08 10:52:28 +0200

    Restructure repo

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-# VizualizationTools
-TFVisualization tools is open source library for feature visualization in deep convolutional networks. It implements multiple techniques for visualization, such as laplace, multiscale, new_dream and deep_dream. 
+# Conveiro
+
+Conveiro is an open source library for feature visualization in deep convolutional networks. It implements multiple techniques for visualization, such as laplace, multiscale,  deep dream and CDFS.
+
 All of these methods are based on:
 * https://distill.pub/2017/feature-visualization/ 
 * https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/tutorials/deepdream/deepdream.ipynb
 
+## Requirements
+
+* Python 3.4 and above
+* Tensorflow (CPU or GPU variant)
+* Numpy
+* Matplotlib
+* OpenCV-Python
+
+## Installation
+
+```
+pip install conveiro
+```
+
 ## Examples
-For examples how to use this library please take a look at jupyter notebooks.
+
+For examples how to use this library please take a look at jupyter notebooks in `doc/` folder.
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages, setup
+import os
+import codecs
+
+from conveiro import __version__
+
+with codecs.open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r', encoding='utf-8') as f:
+    description = f.read()
+
+
+setup(
+    author='',
+    author_email='',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        "Topic :: Scientific/Engineering",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    description='Visualization of filters in convolutional neural networks',
+    include_package_data=True,
+    install_requires=['numpy', 'matplotlib', 'scipy', 'opencv-python'],
+    extras_require = {
+        'gpu' : ['tensorflow-gpu'],
+        'cpu' : ['tensorflow'],
+        'examples' : ['tensornets']
+    },
+    keywords=['CNN', 'neural networks', 'deep dream', 'visualization'],
+    license='Apache License, Version 2.0',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    name='conveiro',
+    packages=find_packages(),
+    python_requires = "~=3.4",
+    platforms=['any'],
+    version=__version__,
+    url='https://github.com/ShowMax/conveiro',
+)
