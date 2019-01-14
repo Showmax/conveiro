1918cc54db86522ff89573f41a27f4f2a8c9d46b
Jan Pipek <jan.pipek@showmax.com>
2019-01-14 12:27:28 +0100

    Module __doc__ for conveiro

--- conveiro/__init__.py
+++ conveiro/__init__.py
@@ -1,1 +1,11 @@
+"""Conveiro
+
+Conveiro (convolutional + oneiro, Greek for "dream") is
+an open source library for feature visualization in
+deep convolutional networks.
+
+It implements multiple techniques for visualization,
+such as laplace, multiscale, deep dream and CDFS.
+"""
+
 __version__ = '0.1'
